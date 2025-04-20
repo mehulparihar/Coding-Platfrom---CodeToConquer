ac2e88cffba74f9738aacc2e2978cb5b939f6f66
mehul parihar <94736804+mehulparihar@users.noreply.github.com>
2025-04-20 21:21:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ import { connectRedis } from "./lib/redis.js";
 import authRoutes from "./routes/auth.route.js"
 import problemsRoutes from "./routes/problems.route.js"
 import submissionRoutes from "./routes/submission.route.js"
-// import { startJudgeWorker } from "./services/judge.service.js";
 import http from 'http';
 import contestRoutes from "./routes/contest.route.js"
 import cron from "./utils/cron.utils.js"
@@ -14,11 +13,14 @@ import battleRoutes from "./routes/battle.route.js"
 import leaderboardRoutes from "./routes/leaderboard.route.js"
 import socketHandler from "./services/liveBattle.service.js";
 import cors from "cors";
+import path from "path";
 
 
 dotenv.config();
 const app = express();
 const httpServer = http.createServer(app);
+const __dirname = path.resolve();
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -36,10 +38,17 @@ app.use("/api/contest", contestRoutes);
 app.use("/api/battles", battleRoutes);
 app.use("/api/leaderboard", leaderboardRoutes);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
+
 const startServer = async () => {
     await connectDB();
     await connectRedis();
-    // startJudgeWorker();
     socketHandler(httpServer);
     httpServer.listen(PORT, '0.0.0.0', () => {
         console.log("Server is running on port", PORT);
