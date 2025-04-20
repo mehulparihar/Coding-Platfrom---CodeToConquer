9591aa423a135e8e89944b577e4c22582d6f8819
mehul parihar <94736804+mehulparihar@users.noreply.github.com>
2025-04-20 23:27:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
-import { connectRedis } from "./lib/redis.js";
 import authRoutes from "./routes/auth.route.js"
 import problemsRoutes from "./routes/problems.route.js"
 import submissionRoutes from "./routes/submission.route.js"
+import { startJudgeWorker } from "./services/judge.service.js";
 import http from 'http';
 import contestRoutes from "./routes/contest.route.js"
 import cron from "./utils/cron.utils.js"
 import battleRoutes from "./routes/battle.route.js"
 import leaderboardRoutes from "./routes/leaderboard.route.js"
 import socketHandler from "./services/liveBattle.service.js";
+
 import cors from "cors";
 import path from "path";
 
@@ -49,6 +50,7 @@ if(process.env.NODE_ENV === "production"){
 const startServer = async () => {
     await connectDB();
     await connectRedis();
+    startJudgeWorker();
     socketHandler(httpServer);
     httpServer.listen(PORT, '0.0.0.0', () => {
         console.log("Server is running on port", PORT);
