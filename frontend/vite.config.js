99faeac6923e191a730c06989426393d855748cd
mehul parihar <94736804+mehulparihar@users.noreply.github.com>
2025-04-20 23:29:42 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,16 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server : {
-    host: '0.0.0.0',
-    port: 5173,
     proxy : {
       "/api" : {
-        target : "http://backend:5000",
+        target : "http://localhost:5000",
         changeOrigin: true,
         rewrite: path => path
       },
       "/socket.io": {
-        target : "http://backend:5000",
+        target : "http://localhost:5000",
         ws: true
       }
     }
